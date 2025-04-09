0b34c5bc4824f120560be8bb886d9b25284f946c
Santhosh2010-ramesh <68174618+Santhosh2010-ramesh@users.noreply.github.com>
2025-04-09 21:46:33 +0530

    Update commonBuild.groovy

--- vars/commonBuild.groovy
+++ vars/commonBuild.groovy
@@ -5,13 +5,11 @@ def call() {
 
     stage('Unit Tests') {
         echo 'Running unit tests...'
-        //sh './gradlew test'
+        // sh './gradlew test'
     }
 
     stage('Deploy') {
         echo 'Deploying to environment...'
-        //sh './deploy.sh dev'
+        // sh './deploy.sh dev'
     }
-
-
 }
